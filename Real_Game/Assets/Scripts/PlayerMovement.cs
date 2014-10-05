9c8b64b0ff5702f1df5c122c5f566629fc2afdbf
afroraydude <rayjo5000@yahoo.com>
2014-10-05 17:35:59 -0400

    Ok...now the player can move

--- Real_Game/Assets/Scripts/PlayerMovement.cs
+++ Real_Game/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	public float moveSpeed;
+	private float maxSpeed = 5f;
 
 	private Vector3 input;
 
@@ -16,6 +17,9 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () 
 	{
 		input = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		print (input);
+		if(rigidbody.velocity.magnitude < maxSpeed) 
+		{
+			rigidbody.AddForce(input * moveSpeed);
+		}
 	}
 }
