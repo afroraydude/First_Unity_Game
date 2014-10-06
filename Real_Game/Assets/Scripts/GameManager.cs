b280a5373f776718e2474bb96260c9d650240421
afroraydude <rayjo5000@yahoo.com>
2014-10-06 19:36:11 -0400

    Stopwatches!

--- Real_Game/Assets/Scripts/GameManager.cs
+++ Real_Game/Assets/Scripts/GameManager.cs
@@ -4,9 +4,27 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	public static int currentScore;
 	public static int highScore;
-
 	public static int currentLevel = 0;
 	public static int unlockedLevel;
+	public Rect stopwatchRect;
+	public Rect stopwatchBoxRect;
+
+	public GUISkin skin;
+
+	public float startTime;
+	private string currentTime;
+
+	void Update()
+	{
+		startTime += Time.deltaTime;
+		currentTime = string.Format ("{0:0.0}", startTime);
+	}
+	void OnGUI()
+	{
+		GUI.skin = skin;
+		GUI.Box (stopwatchBoxRect, "");
+		GUI.Label(stopwatchRect, currentTime, skin.GetStyle ("Stopwatch"));
+	}
 
 	public static void CompleteLevel()
 	{
--- Real_Game/Assets/Scripts/MainMenu.cs
+++ Real_Game/Assets/Scripts/MainMenu.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary; 
 
 public class MainMenu : MonoBehaviour 
 {
-
+	public GUISkin skin;
 
 	void OnGUI()
 	{
-		// offset width, offset height, object width, object height
-		if (GUI.Button(new Rect (10,10,100,45), "Play"))
+		
+		GUI.skin = skin;
+		/** 
+		 * Offset x, offset y, object width, object height
+		 * To set the offset height of a gui item that is under another
+		 * gui item correctly, add 20 to the above gui item's 
+		 * height/y offset.
+		*/
+		GUI.Label (new Rect (10,10,440,45), "Afroraydude's Puzzle Game");
+		GUI.Label (new Rect (10,65,440,45), "Pre Alpha Version 1");
+		if (GUI.Button(new Rect (10,150,100,45), "Play"))
 		{
 			print ("Button 'Play' has been pressed!");
 			GameManager.CompleteLevel ();
 		}
-		if (GUI.Button(new Rect (10,65,100,45), "Quit"))
+		if (GUI.Button(new Rect (10,205,100,45), "Quit"))
 		{
 			print ("Button 'Quit' has been pressed!");
 			Application.Quit ();
