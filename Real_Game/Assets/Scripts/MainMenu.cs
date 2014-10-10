53ce105fcb6a375d258f25d3f028a7d5c6d4db89
afroraydude <rayjo5000@yahoo.com>
2014-10-10 10:03:42 -0400

    Updated MainMenu.cs
    Changed lines 10, 16, 27-29, 34
    Added Lines 39, 32-33, 21, 9-12, 14-15

--- Real_Game/Assets/Scripts/MainMenu.cs
+++ Real_Game/Assets/Scripts/MainMenu.cs
@@ -6,14 +6,19 @@ public class MainMenu : MonoBehaviour
 {
 
 	public GUISkin skin;
+	public Rect title = 10,10,440,45;
+	public Rect version = 10,65,440,45;
+	public Rect playButton = 10,150,100,45;
+	public Rect quitButton = 10,205,100,45;
 	public GameManager manager;
-
+	
+	// Mostly only used to read the components in 
 	void Start () 
 	{
 		manager = manager.GetComponent<GameManager>();
 		//Destroy(gameObject);
 	}
-
+	// GUI 
 	void OnGUI()
 	{
 		
@@ -24,14 +29,14 @@ public class MainMenu : MonoBehaviour
 		 * gui item correctly, add 20 to the above gui item's 
 		 * height/y offset.
 		*/
-		GUI.Label (new Rect (10,10,440,45), "Afroraydude's Puzzle Game");
-		GUI.Label (new Rect (10,65,440,45), "Pre Alpha Version 2");
-		if (GUI.Button(new Rect (10,150,100,45), "Play"))
+		GUI.Label (title, "Afroraydude's Puzzle Game");
+		GUI.Label (version, "Pre Alpha Version 2");
+		if (GUI.Button(playButton, "Play"))
 		{
 			print ("Button 'Play' has been pressed!");
 			manager.MainMenuToLevelOne();
 		}
-		if (GUI.Button(new Rect (10,205,100,45), "Quit"))
+		if (GUI.Button(quitButton, "Quit"))
 		{
 			print ("Button 'Quit' has been pressed!");
 			Application.Quit ();
