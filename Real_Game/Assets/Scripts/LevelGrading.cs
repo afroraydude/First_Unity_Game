f52753454d8a8a3287ebf1e40d7e99337f709c56
afroraydude <rayjo5000@yahoo.com>
2015-01-08 11:38:49 -0500

    Update LevelGrading.cs

--- Real_Game/Assets/Scripts/LevelGrading.cs
+++ Real_Game/Assets/Scripts/LevelGrading.cs
@@ -2,3 +2,14 @@ using UnityEngine;
 using System.Collections;
 
 public class LevelGrading : MonoBehaviour {
+        //public int;
+        
+        void Start() {
+                
+        }
+        
+        void Update() {
+                
+        }
+        
+}
