950e4efa802ea01f703373bbcaf623c6fb5f2d92
afroraydude <rayjo5000@yahoo.com>
2015-01-09 17:31:14 -0500

    Pre-Alpha Version 4

---
+++ Real_Game/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+	public GameObject target;
+	public float damping = 1;
+	Vector3 offset;
+	
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+
+	void LateUpdate() {
+		  
+	}
+}
