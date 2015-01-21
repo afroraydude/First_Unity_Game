cf8780991b3dccb19d451a7e8126ada040e02f0e
afroraydude <rayjo5000@yahoo.com>
2015-01-21 14:03:05 -0500

    Update LevelGrading.cs

--- Real_Game/Assets/Scripts/LevelGrading.cs
+++ Real_Game/Assets/Scripts/LevelGrading.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class LevelGrading : MonoBehaviour {
-        //public int;
+        public float finalScore; // This is the final score you recieve
+        public string finalScoreLetter; // 
+        public float[] timeGrading = new float[5]; // 5 for the 5 grades (A, B, C D, F)
+        public int[] deathGrading = new int[5]; // 5 for the 5 grades (A, B, C D, F)
+        
         
         void Start() {
                 
         }
         
         void Update() {
-                
+           //Probably would be empty     
+        }
+        
+        void OnGUI() {
+                // I might want to do the GUI
         }
         
 }
+
