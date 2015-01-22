b4d77b35eebaa1bc427b86258653b74822ed89a8
afroraydude <rayjo5000@yahoo.com>
2015-01-22 12:56:56 -0500

    Update GameManager.cs

--- Real_Game/Assets/Scripts/GameManager.cs
+++ Real_Game/Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour {
 	// Score based stuff
 	public float highScore = 0f;
 	
-	//Level based stuff
+	// Level based stuff
 	public int currentLevel = 0;
 	public int unlockedLevels;
 	public int LevelGradingID = 6;
@@ -24,8 +24,7 @@ public class GameManager : MonoBehaviour {
 	public string highTime;
 	
 	// Once the level loads this happens
-	void Start()
-	{
+	void Start() {
 		// DontDestroyOnLoad(gameObject);
 		/**
 		* 
@@ -40,8 +39,7 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	// This is ran every tick
-	void Update()
-	{
+	void Update() {
 		//This records the time it took to complete the level
 		startTime += Time.deltaTime;
 		//This puts it into a string so that it can be viewed on the GUI
@@ -49,8 +47,7 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	// GUI goes here
-	void OnGUI()
-	{
+	void OnGUI() {
 		GUI.skin = skin;
 		GUI.Box (stopwatchBoxRect, "");
 		GUI.Label(stopwatchRect, currentTime, skin.GetStyle ("Stopwatch"));
@@ -58,40 +55,30 @@ public class GameManager : MonoBehaviour {
 		GUI.Box (highScoreBox, "");
 	}
 	
-	
-
-	/**
-	* Alternative to the Maine Menu having to save information
-	*/
-	
-	void MainMenuToLevelOne()
-	{
+	public void MainMenuToLevelOne() {
 		currentLevel = 1;
 		PlayerPrefs.SetInt("LevelsCompleted", currentLevel);
 		PlayerPrefs.Save();
 		Application.LoadLevel(currentLevel);
 	}
 
-	void BackToMainMenu()
-	{
+	public void BackToMainMenu() {
 		currentLevel = 0;
 		Application.LoadLevel(currentLevel);
 	}
+	
 	// For when the player completes a level
 	// TODO: Fix issue in flash were it does not care if the high score is > or < the startTime, it always overrides it.
-	void CompleteLevel()
-	{
+	public void CompleteLevel() {
 		unlockedLevels = PlayerPrefs.GetInt("LevelsCompleted");
 		
-		if(highScore > startTime || highScore == 0)
-		{
+		if(highScore > startTime || highScore == 0) {
 			highScore = startTime;
 			highTime = string.Format ("{0:0.0}", highScore);
 			PlayerPrefs.SetString("Level" + currentLevel.ToString() + "Score", highTime);
 		}
 
-		if (currentLevel < 5)
-		{
+		if (currentLevel < 5) {
 			if (unlockedLevels < currentLevel) {
 				PlayerPrefs.SetInt("LevelsCompleted", currentLevel);
 				PlayerPrefs.GetInt("LevelsCompleted");
@@ -100,19 +87,18 @@ public class GameManager : MonoBehaviour {
 			Application.LoadLevel(LevelGradingID);
 		}
 
-		else
-		{
+		else {
 			print ("Please increase level amount.");
 		}
 	}
 	
-	void AfterGrading()
-	{
+	// After LevelGrading is done, this happens
+	public void AfterGrading() {
 		currentLevel +=1;
 		Application.LoadLevel(currentLevel);
 	}
 	
-	void StartGame() {
+	public void StartGame() {
 		currentLevel = 0;
 	}
 }
