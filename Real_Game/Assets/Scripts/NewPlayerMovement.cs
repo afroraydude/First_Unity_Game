b7e31dbedb1bddcc634fb9e2ca1f142af13248f2
afroraydude <rayjo5000@yahoo.com>
2015-03-06 19:23:18 -0500

    [Different] New player mesh (Not Implemented!)

---
+++ Real_Game/Assets/Scripts/NewPlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPlayerMovement : MonoBehaviour {
+
+	public GameManager manager;
+
+	public float moveSpeed;
+	private float maxSpeed = 5f;
+	public int deathCount;
+	public float killY = -1.5f;
+
+	public GameObject deathParticals;
+
+	private Vector3 input;
+	private Vector3 spawn;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spawn = transform.position;
+		manager = manager.GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		input = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		if(GetComponent<Rigidbody>().velocity.magnitude < maxSpeed) 
+		{
+			GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+		}
+		if (transform.position.y <= killY)
+		{
+			Die ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "Goal")
+		{
+			manager.CompleteLevel();
+
+		}
+		if (other.transform.tag == "Invisible") {
+			Die();
+		}
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.transform.tag == "Enemy") {
+			Die ();
+		}
+	}
+	
+
+	void Die() {
+		deathCount += 1;
+		Instantiate(deathParticals, transform.position, Quaternion.identity);
+		transform.position = spawn;
+	}
+	
+}
