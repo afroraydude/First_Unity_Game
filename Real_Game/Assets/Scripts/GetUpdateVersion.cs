7e9a1f7649f66bc598811b630457ebed418e40e0
afroraydude <rayjo5000@yahoo.com>
2015-03-16 10:58:01 -0400

    Update GetUpdateVersion.cs

--- Real_Game/Assets/Scripts/GetUpdateVersion.cs
+++ Real_Game/Assets/Scripts/GetUpdateVersion.cs
@@ -2,22 +2,51 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class WebFileLoader : MonoBehaviour {
+public class GetUpdateVersion : MonoBehaviour {
 
 	public WWW update;
+	public Button updateButton;
+	public Text updateButtonText;
+	public string updateURL;
+	public float version;
+	public string gotVersionText;
+	public float gotVersion;
 	
 	// Use this for initialization
 	void Start () {
-
+		updateButtonText.resizeTextMaxSize = 14;
+		updateButtonText.resizeTextForBestFit = true;
+		/**
+		StartCoroutine( Do() );
+      		//or the less efficient version that takes a string, but is limited to a single parameter.:
+      		StartCoroutine("Do" , parameter);
+      		//The advantage to the string version is that you can call stop coroutine:
+      		StopCoroutine("Do");
+		*/
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+		StartCoroutine (GetVersion ());
+		StopCoroutine (GetVersion ());
 	}
 
-	IEnumerator GetURL (string url) {
-		www = new WWW (url);
-		yield return www;
+	IEnumerator GetUpdateData (string url) {
+		update = new WWW (url);
+		yield return update;
+		gotVersionText = updateWWW.text.ToString();
+		print (gotVersionText);
+		gotVersion = float.Parse (gotVersionText);
+		print (gotVersion);
+		CheckIfVersionIsLatest ();
+	}
+
+	void CheckIfVersionIsLatest () {
+		if (gotVersion == version || Application.isWebPlayer) {
+			updateButton.enabled = false;
+			updateButtonText.text = "Up to Date";
+		} else {
+			// end
+		}
 	}
 }
